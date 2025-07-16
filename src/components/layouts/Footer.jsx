9723339087652f180297ac8139db39cb63f155d6
barakahtrust <barakahtrustbd@gmail.com>
2025-07-16 15:31:20 +0600

    done

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -4,12 +4,12 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <>
-      <Link to={"/"}>
-        <li className="hover:text-[#262626] text-[#767676] text-sm">Home</li>
-      </Link>
-      <Link to={"/shop"}>
-        <li className="hover:text-[#262626] text-[#767676] text-sm">Shop</li>
-      </Link>
+      <li className="hover:text-[#262626] text-[#767676] text-sm">
+        <Link to={"/"}>Home</Link>
+      </li>
+      <li className="hover:text-[#262626] text-[#767676] text-sm">
+        <Link to={"/shop"}>Shop</Link>
+      </li>
     </>
   );
 };
