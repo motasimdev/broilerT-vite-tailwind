7bff5a9233ae931a0bcae9e06fe46285dcef2fb9
motasimdev <themotasimdev@gmail.com>
2025-08-27 16:15:48 +0600

    header change

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -3,12 +3,18 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <>
-      <li className="hover:text-[#262626] text-[#767676] text-sm">
-        <Link to={"/"}>Home</Link>
-      </li>
-      <li className="hover:text-[#262626] text-[#767676] text-sm">
-        <Link to={"/shop"}>Shop</Link>
-      </li>
+      <Container>
+        <div className="py-6 bg-amber-100">
+          <ul className="flex items-center justify-center gap-x-3">
+            <li className="hover:text-[#262626] text-[#767676] text-sm">
+              <Link to={"/"}>Home</Link>
+            </li>
+            <li className="hover:text-[#262626] text-[#767676] text-sm">
+              <Link to={"/shop"}>Shop</Link>
+            </li>
+          </ul>
+        </div>
+      </Container>
     </>
   );
 };
