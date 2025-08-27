009779dc16550784ade70a89943f2f4f650d366a
motasimdev <themotasimdev@gmail.com>
2025-08-27 16:20:17 +0600

    home modified

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,14 +1,12 @@
-import Container from "../Container"
-import Flex from "../Flex"
-
+import Container from "../Container";
+import Flex from "../Flex";
+import Banner from "../layouts/Banner";
 
 const Home = () => {
   return (
-    <div className="">
-        <Container>
-            <Flex>
-                <h1>this is home</h1>
-                {/* 
+    <>
+      <Banner />
+      {/* 
                 cmd run--
 
                 del package-lock.json && rmdir /s /q node_modules && npm install 
@@ -18,10 +16,8 @@ const Home = () => {
                 rm -f package-lock.json && rm -rf node_modules && npm install
 
                 */}
-            </Flex>
-        </Container>
-    </div>
-  )
-}
+    </>
+  );
+};
 
-export default Home
+export default Home;
